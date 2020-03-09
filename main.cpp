a1888d52437a8dab20c335d20984adaf60a932bb
AginS <agins.main@gmail.com>
2020-03-09 14:54:34 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,9 +4,9 @@
 using namespace std;
 
 int main() {
-    DateTime dt(2020, 02, 24, 0, 0, 0);
+    DateTime dt(2020, 02, 24, 10, 5, 43);
     //dt.addDateTime(0, 02, 25, 23, 56, 58);
-    cout << dt.subtractDateTime(0,5, 10, 0,0,0) << endl;
+    cout << dt.subtractDateTime(2,5, 10, 6,7,13) << endl;
     //dt.addDateTime(AT_DAY, 5);
     //cout << dt.getDateTime();
     return 0;
