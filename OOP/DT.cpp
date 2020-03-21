d98a5c0d95136b727b85a08eb5a453a737aa7be6
AginS <agins.main@gmail.com>
2020-03-21 12:34:10 +0300

    Update DT.cpp

--- OOP/DT.cpp
+++ OOP/DT.cpp
@@ -236,31 +236,6 @@ bool DateTime::checkDateTime(int year, int mon, int day, int hour, int min, int 
     }
 }
 
-int DateTime::dayInMonth(int month, bool isLeapers) {
-    switch (month)
-    {
-        case 1:
-        case 3:
-        case 5:
-        case 7:
-        case 8:
-        case 10:
-        case 12:
-            return 31;
-        case 2:
-            if (isLeapers)
-                return 29;
-            else
-                return 28;
-            // апрель, июнь, сентябрь и ноябрь
-        case 4:
-        case 6:
-        case 9:
-        case 11:
-            return 30;
-    }
-}
-
 bool DateTime::_is_leap(int year) {
     return (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0));
 }
